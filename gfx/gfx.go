9ad6d70afe698f0ee0cafb2e7aa831a39eba0e2a
Gabor Torok <gtorok@lbl.gov>
2020-04-19 20:58:30 -0700

    working on opengl version of gfx

--- gfx/gfx.go
+++ gfx/gfx.go
@@ -2,14 +2,64 @@ package gfx
 
 import (
 	"fmt"
+	"log"
+	"math/rand"
+	"runtime"
+	"strings"
 	"sync"
 
-	"github.com/veandco/go-sdl2/sdl"
+	"github.com/go-gl/gl/v4.1-core/gl"
+	"github.com/go-gl/glfw/v3.2/glfw"
 )
 
-const SCALE = 2.0
-const WIDTH = 320
-const HEIGHT = 200
+const (
+	width  = 320
+	height = 200
+	scale  = 2
+
+	vertexShaderSource = `
+		#version 410
+		layout (location = 0) in vec3 aPos;
+		layout (location = 1) in vec3 aColor;
+		layout (location = 2) in vec2 aTexCoord;
+
+		out vec3 ourColor;
+		out vec2 TexCoord;
+
+		void main() {
+			gl_Position = vec4(aPos, 1.0);
+			ourColor = aColor;
+			TexCoord = aTexCoord;
+		}
+	` + "\x00"
+
+	fragmentShaderSource = `
+		#version 410
+		out vec4 FragColor;
+  
+		in vec3 ourColor;
+		in vec2 TexCoord;
+
+		uniform sampler2D ourTexture;
+
+		void main()
+		{
+			FragColor = texture(ourTexture, TexCoord);
+		}
+	` + "\x00"
+)
+
+var (
+	screen = []float32{
+		// xyz		color		texture coords
+		-1, 1, 0, 1, 1, 1, 0, 0,
+		-1, -1, 0, 1, 1, 1, 0, 1,
+		1, -1, 0, 1, 1, 1, 1, 1,
+		1, 1, 0, 1, 1, 1, 1, 0,
+		-1, 1, 0, 1, 1, 1, 0, 0,
+		1, -1, 0, 1, 1, 1, 1, 1,
+	}
+)
 
 // GfxTextMode is a 40x25 char text mode, 16 color background, 16 color foreground
 const GfxTextMode = 0
@@ -25,50 +75,28 @@ type Gfx struct {
 	// the graphics mode
 	Mode uint8
 	// the video memory
-	PixelMemory [HEIGHT * WIDTH]uint8
+	PixelMemory [width * height]uint8
 	// character memory
-	TextMemory [HEIGHT / 8 * WIDTH / 8]uint8
+	TextMemory [width / 8 * height / 8]uint8
 	// the color palette (16 rgb values)
 	Colors [16 * 3]uint8
 	// The background color (used by some modes)
 	BackgroundColor uint8
 
-	Window      *sdl.Window
-	Renderer    *sdl.Renderer
-	Texture     *sdl.Texture
-	PixelFormat *sdl.PixelFormat
-	Lock        sync.Mutex
-	Pixels      []byte
-	Pitch       int
+	Lock    sync.Mutex
+	Window  *glfw.Window
+	Program uint32
+	Vao     uint32
 }
 
 // NewGfx lets you create a new Gfx video card
 func NewGfx() *Gfx {
-	if err := sdl.Init(sdl.INIT_EVERYTHING); err != nil {
-		panic(err)
-	}
-
-	window, renderer, err := sdl.CreateWindowAndRenderer(int32(WIDTH*SCALE), int32(HEIGHT*SCALE), sdl.RENDERER_ACCELERATED|sdl.RENDERER_PRESENTVSYNC)
-	if err != nil {
-		panic(err)
-	}
-
-	renderer.SetScale(SCALE, SCALE)
-	renderer.SetLogicalSize(WIDTH, HEIGHT)
-
-	pixelFormat := &sdl.PixelFormat{
-		Format: sdl.PIXELFORMAT_RGB888,
-	}
-	texture, err := renderer.CreateTexture(pixelFormat.Format, sdl.TEXTUREACCESS_STREAMING, WIDTH, HEIGHT)
-	if err != nil {
-		panic(err)
-	}
-
 	gfx := &Gfx{
 		Mode:        GfxTextMode,
-		PixelMemory: [HEIGHT * WIDTH]uint8{},
-		TextMemory:  [HEIGHT / 8 * WIDTH / 8]uint8{},
+		PixelMemory: [width * height]uint8{},
+		TextMemory:  [width / 8 * height / 8]uint8{},
 		Colors: [16 * 3]uint8{
+			// C64 colors :-)
 			0x00, 0x00, 0x00,
 			0xff, 0xff, 0xff,
 			0x88, 0x20, 0x00,
@@ -87,66 +115,175 @@ func NewGfx() *Gfx {
 			0xb8, 0xb8, 0xb8,
 		},
 		BackgroundColor: 0,
-		Window:          window,
-		Renderer:        renderer,
-		Texture:         texture,
-		PixelFormat:     pixelFormat,
-		Lock:            sync.Mutex{},
-		Pixels:          nil,
-		Pitch:           0,
+
+		Lock:    sync.Mutex{},
+		Window:  initGlfw(),
+		Program: initOpenGL(),
+		Vao:     makeVao(),
 	}
+
+	runtime.LockOSThread()
 
 	return gfx
 }
 
+// initGlfw initializes glfw and returns a Window to use.
+func initGlfw() *glfw.Window {
+	if err := glfw.Init(); err != nil {
+		panic(err)
+	}
+	glfw.WindowHint(glfw.Resizable, glfw.True)
+	glfw.WindowHint(glfw.ContextVersionMajor, 4)
+	glfw.WindowHint(glfw.ContextVersionMinor, 1)
+	glfw.WindowHint(glfw.OpenGLProfile, glfw.OpenGLCoreProfile)
+	glfw.WindowHint(glfw.OpenGLForwardCompatible, glfw.True)
+
+	window, err := glfw.CreateWindow(width*scale, height*scale, "Benji4000", nil, nil)
+	if err != nil {
+		panic(err)
+	}
+	window.MakeContextCurrent()
+
+	return window
+}
+
+// initOpenGL initializes OpenGL and returns an intiialized program.
+func initOpenGL() uint32 {
+	if err := gl.Init(); err != nil {
+		panic(err)
+	}
+	version := gl.GoStr(gl.GetString(gl.VERSION))
+	log.Println("OpenGL version", version)
+
+	vertexShader, err := compileShader(vertexShaderSource, gl.VERTEX_SHADER)
+	if err != nil {
+		panic(err)
+	}
+
+	fragmentShader, err := compileShader(fragmentShaderSource, gl.FRAGMENT_SHADER)
+	if err != nil {
+		panic(err)
+	}
+
+	prog := gl.CreateProgram()
+	gl.AttachShader(prog, vertexShader)
+	gl.AttachShader(prog, fragmentShader)
+	gl.LinkProgram(prog)
+	return prog
+}
+
+func compileShader(source string, shaderType uint32) (uint32, error) {
+	shader := gl.CreateShader(shaderType)
+
+	csources, free := gl.Strs(source)
+	gl.ShaderSource(shader, 1, csources, nil)
+	free()
+	gl.CompileShader(shader)
+
+	var status int32
+	gl.GetShaderiv(shader, gl.COMPILE_STATUS, &status)
+	if status == gl.FALSE {
+		var logLength int32
+		gl.GetShaderiv(shader, gl.INFO_LOG_LENGTH, &logLength)
+
+		log := strings.Repeat("\x00", int(logLength+1))
+		gl.GetShaderInfoLog(shader, logLength, nil, gl.Str(log))
+
+		return 0, fmt.Errorf("failed to compile %v: %v", source, log)
+	}
+
+	return shader, nil
+}
+
+// makeVao initializes and returns a vertex array from the points provided.
+func makeVao() uint32 {
+	var vbo uint32
+	gl.GenBuffers(1, &vbo)
+	gl.BindBuffer(gl.ARRAY_BUFFER, vbo)
+	gl.BufferData(gl.ARRAY_BUFFER, 4*len(screen), gl.Ptr(screen), gl.STATIC_DRAW)
+
+	var vao uint32
+	gl.GenVertexArrays(1, &vao)
+	gl.BindVertexArray(vao)
+	gl.BindBuffer(gl.ARRAY_BUFFER, vbo)
+	var offset int
+
+	// position attribute
+	gl.VertexAttribPointer(0, 3, gl.FLOAT, false, 8*4, gl.PtrOffset(offset))
+	gl.EnableVertexAttribArray(0)
+	offset += 3 * 4
+
+	// color attribute
+	gl.VertexAttribPointer(1, 3, gl.FLOAT, false, 8*4, gl.PtrOffset(offset))
+	gl.EnableVertexAttribArray(1)
+	offset += 3 * 4
+
+	// texture coord attribute
+	gl.VertexAttribPointer(2, 2, gl.FLOAT, false, 8*4, gl.PtrOffset(offset))
+	gl.EnableVertexAttribArray(2)
+	offset += 2 * 4
+
+	return vao
+}
+
 func (gfx *Gfx) MainLoop() {
-	defer sdl.Quit()
-	defer gfx.Window.Destroy()
-	defer gfx.Renderer.Destroy()
-	defer gfx.Texture.Destroy()
+	defer glfw.Terminate()
 
-	running := true
-	var current_time, fps_last_time uint32 = 0, sdl.GetTicks()
-	var c int
-	var fps float32
-
-	src := &sdl.Rect{
-		X: 0,
-		Y: 0,
-		W: WIDTH,
-		H: HEIGHT,
+	var pixels [320 * 200 * 3]byte
+	i := 0
+	for y := 0; y < 200; y++ {
+		for x := 0; x < 320; x++ {
+			pixels[i] = byte((float32(x) / 320.0) * (float32(y) / 200.0) * 255.0)
+			pixels[i+1] = 0x00
+			pixels[i+2] = 0x00
+			i += 3
+		}
 	}
-	dst := &sdl.Rect{
-		X: 0,
-		Y: 0,
-		W: WIDTH,
-		H: HEIGHT,
-	}
-	for running {
-		current_time = sdl.GetTicks()
-		c++
+	// fmt.Printf("pixels=%v\n", pixels[0:10])
 
-		// FPS counter math:
-		if current_time-fps_last_time > 500 {
-			fps = float32(c*1000) / float32(current_time-fps_last_time)
-			gfx.Window.SetTitle(fmt.Sprintf("fps: %.2f", fps))
-			fps_last_time = current_time
-			c = 0
+	// texture setup
+	var texture uint32
+	gl.GenTextures(1, &texture)
+	gl.BindTexture(gl.TEXTURE_2D, texture)
+	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.REPEAT)
+	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.REPEAT)
+	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR)
+	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR)
+	gl.TexImage2D(gl.TEXTURE_2D, 0, gl.RGB, width, height, 0, gl.RGB, gl.UNSIGNED_BYTE, gl.Ptr(&pixels[0]))
+	gl.GenerateMipmap(gl.TEXTURE_2D)
+
+	// bind to shader
+	gl.UseProgram(gfx.Program)
+	gl.Uniform1i(gl.GetUniformLocation(gfx.Program, gl.Str("ourTexture\x00")), 0)
+
+	var lastTime, delta float64
+	var nbFrames int
+	for !gfx.Window.ShouldClose() {
+		currentTime := glfw.GetTime()
+		delta = currentTime - lastTime
+		nbFrames++
+		if delta >= 1.0 { // If last cout was more than 1 sec ago
+			gfx.Window.SetTitle(fmt.Sprintf("FPS: %.2f", float64(nbFrames)/delta))
+			nbFrames = 0
+			lastTime = currentTime
 		}
 
-		gfx.Lock.Lock()
-		gfx.Renderer.Copy(gfx.Texture, src, dst)
-		gfx.Renderer.Present()
-		gfx.Lock.Unlock()
-
-		for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
-			switch event.(type) {
-			case *sdl.QuitEvent:
-				println("Quit")
-				running = false
-				break
-			}
+		// blip the video ram to texture (random for now)
+		for i := 0; i < len(pixels); i++ {
+			pixels[i] = byte(rand.Intn(255))
 		}
+		gl.TexSubImage2D(gl.TEXTURE_2D, 0, 0, 0, width, height, gl.RGB, gl.UNSIGNED_BYTE, gl.Ptr(&pixels[0]))
+
+		gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+		gl.ActiveTexture(gl.TEXTURE0)
+		gl.BindTexture(gl.TEXTURE_2D, texture)
+		gl.UseProgram(gfx.Program)
+
+		gl.BindVertexArray(gfx.Vao)
+		gl.DrawArrays(gl.TRIANGLES, 0, int32(len(screen)/8))
+
+		glfw.PollEvents()
+		gfx.Window.SwapBuffers()
 	}
 }
 
@@ -196,64 +333,32 @@ func (gfx *Gfx) renderMultiColorMode() {
 
 func (gfx *Gfx) StartUpdate() error {
 	gfx.Lock.Lock()
-	pixels, pitch, err := gfx.Texture.Lock(nil)
-	if err != nil {
-		return err
-	}
-	// gfx.Texture.SetBlendMode(sdl.BLENDMODE_NONE)
+	// pixels, pitch, err := gfx.Texture.Lock(nil)
+	// if err != nil {
+	// 	return err
+	// }
+	// // gfx.Texture.SetBlendMode(sdl.BLENDMODE_NONE)
 
-	gfx.Pixels = pixels
-	gfx.Pitch = pitch
+	// gfx.Pixels = pixels
+	// gfx.Pitch = pitch
 	return nil
 }
 
 func (gfx *Gfx) EndUpdate() error {
-	gfx.Texture.Unlock()
+	// gfx.Texture.Unlock()
 	gfx.Lock.Unlock()
 	return nil
 }
 
-func (gfx *Gfx) SetPixel(x, y int, ch uint8, fg uint8, bg uint8) {
+func (gfx *Gfx) SetPixel(x, y int, ch, fg, bg uint8) {
 
 	switch {
 	case gfx.Mode == GfxTextMode:
 		panic("Implement me!")
 	case gfx.Mode == GfxHiresMode:
-		gfx.SetPixelHires(x, y, fg)
+		// gfx.SetPixelHires(x, y, fg)
 	case gfx.Mode == GfxMultiColorMode:
-		gfx.SetPixelMulti(x, y, fg)
+		// gfx.SetPixelMulti(x, y, fg)
 	}
 
-}
-
-func (gfx *Gfx) SetPixelHires(x, y int, fg uint8) {
-	if x < 0 || x >= WIDTH || y < 0 || y >= HEIGHT {
-		return
-	}
-
-	r, g, b := gfx.Colors[(fg%8)*3], gfx.Colors[(fg%8)*3+1], gfx.Colors[(fg%8)*3+2]
-
-	pixelPosition := y*gfx.Pitch + x*4
-	gfx.Pixels[pixelPosition] = r
-	gfx.Pixels[pixelPosition+1] = g
-	gfx.Pixels[pixelPosition+2] = b
-	gfx.Pixels[pixelPosition+3] = 0xff
-}
-
-func (gfx *Gfx) SetPixelMulti(x, y int, fg uint8) {
-	if x < 0 || x >= WIDTH/2 || y < 0 || y >= HEIGHT {
-		return
-	}
-
-	r, g, b := gfx.Colors[fg*3], gfx.Colors[fg*3+1], gfx.Colors[fg*3+2]
-
-	pixelPosition := y*gfx.Pitch + x*8
-	gfx.Pixels[pixelPosition] = r
-	gfx.Pixels[pixelPosition+1] = g
-	gfx.Pixels[pixelPosition+2] = b
-	gfx.Pixels[pixelPosition+3] = 0xff
-	gfx.Pixels[pixelPosition+4] = r
-	gfx.Pixels[pixelPosition+5] = g
-	gfx.Pixels[pixelPosition+6] = b
-	gfx.Pixels[pixelPosition+7] = 0xff
 }
